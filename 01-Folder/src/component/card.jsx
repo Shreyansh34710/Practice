c6fe4ef4e9407d0b527fdf2c4d6c3e79aa18cbe6
Shreyansh34710 <adi29shreyansh@gmail.com>
2026-06-21 15:39:37 +0530

    new changes

---
+++ 01-Folder/src/component/card.jsx
@@ -0,0 +1,12 @@
+
+
+function card(){
+  return (
+    <div className='card'>
+      <h1>hello</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus, expedita!</p>
+    </div>
+  )
+}
+
+export default card
